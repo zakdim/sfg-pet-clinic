f964e6d236db7d78aab4d9eb783ad13039d0cb72
Dmitri Zakharov <dmitri.zakharov@desjardins.com>
2021-05-09 19:47:13 -0400

    s03-46 Adding POJOs
    Closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * Created by developer on 2021-05-09.
+ */
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * Created by developer on 2021-05-09.
+ */
+public class Vet extends Person {
+}
