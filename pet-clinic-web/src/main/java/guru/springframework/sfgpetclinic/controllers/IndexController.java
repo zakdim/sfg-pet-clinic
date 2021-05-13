a37deb9444d996269098b39a764a737171de26d4
Dmitri Zakharov <dmitri.zakharov@desjardins.com>
2021-05-12 23:43:11 -0400

    s04-64 Spring Pet Clinic - Creating Index Page and Controller
    Closes #8

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by developer on 2021-05-12.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+
+        return "index";
+    }
+}
