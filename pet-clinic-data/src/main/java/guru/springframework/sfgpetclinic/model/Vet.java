4c83fc49b0ac630c5a1adcf9e9db8cc4b9671631
Dmitri Zakharov <dmitri.zakharov@desjardins.com>
2021-05-27 22:51:55 -0400

    s07-120 Spring Pet Clinic - Create Vet Specialty Entity, Add to Vet
    Closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,7 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by developer on 2021-05-09.
  */
 public class Vet extends Person {
+
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
