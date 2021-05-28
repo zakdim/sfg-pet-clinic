dfb89b6cc8cf46e5849dbe5a182b36b217f59b7e
Dmitri Zakharov <dmitri.zakharov@desjardins.com>
2021-05-27 22:44:56 -0400

    s07-118 Spring Pet Clinic - Create Pet Type, Pet and Visit Entities
    Closes #26

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by developer on 2021-05-09.
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
