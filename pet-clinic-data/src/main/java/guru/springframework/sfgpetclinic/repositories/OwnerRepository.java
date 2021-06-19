6d8d8d53dfb84b6b0d1999eaebf2295427263bb5
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2021-06-19 17:58:30 -0400

    s08-151 Spring Pet Clinic - Add Spring Data JPA Owner Service
    Closes #40

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.repository.CrudRepository;
  * Created by developer on 2021-06-16.
  */
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
