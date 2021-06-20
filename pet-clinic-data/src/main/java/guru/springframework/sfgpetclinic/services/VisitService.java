1a4bc0778f22a61733d7ebf5601ae13cd5ad6815
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2021-06-20 16:37:22 -0400

    s08-157 Spring Pet Clinic - Add Map Based Visit Service
    Edited this commit using: git rebase -i HEAD~4
    Closes #46

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+
+/**
+ * Created by dmitri on 2021-06-20.
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
